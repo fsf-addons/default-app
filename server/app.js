7e141e75776bf103af4a949cc71fd8e2be09e8ff
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-18 09:32:30 +0800

    Updated body parser usage to fix warnings

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ var bodyParser = require('body-parser');
 const NODE_PORT = process.env.NODE_PORT || 3000;
 
 var app = express();
-app.use(bodyParser());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + "/../client/"));
 
 app.listen(NODE_PORT, function () {
